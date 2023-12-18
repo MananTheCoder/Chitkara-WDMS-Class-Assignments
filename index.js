29011bf8128befbfbb27d642442fda8c903f22c9
Umesh <97531471+umeshyadav7988@users.noreply.github.com>
2023-12-18 20:28:37 +0530

    Add files via upload
    Express_assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
