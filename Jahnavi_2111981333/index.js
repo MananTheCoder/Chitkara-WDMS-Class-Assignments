d1fca3030fb8951dc7baac93629d911c7cab9e21
jahnavi-22 <110335444+jahnavi-22@users.noreply.github.com>
2023-12-18 15:40:42 +0530

    Updated index.js

--- Jahnavi_2111981333/index.js
+++ Jahnavi_2111981333/index.js
@@ -1,12 +1,14 @@
-
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+  // res.send('Hello World!');
+  res.sendFile(path.join(__dirname,"public" ,"index.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening on port ${port}`);
+}); 
