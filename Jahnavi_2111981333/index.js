83a544d7f6c9b0538e99b44e3e9ea328fff73f32
jahnavi-22 <jahnavi.22053@gmail.com>
2023-12-18 15:32:18 +0530

    Express integration

---
+++ Jahnavi_2111981333/index.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
