d962d1a1d8915196f12a1d7b3addd749dda7fccf
Bart <madhavgoyal2k2@gmail.com>
2023-12-19 00:44:51 +0530

    improved folder structure

--- Madhav_Goyal__2011981262/current_project/index.js
+++ Madhav_Goyal__2011981262/current_project/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// use current directory as static
-app.use(express.static(__dirname));
+// use static
+app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
-app.get('/about', (req, res) => res.sendFile(__dirname + '/about.html'));
+app.get('/about', (req, res) => res.sendFile(__dirname + '/public/about.html'));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
