1fb25287e6d40c3d35a7819094d9b9a66fd8b217
602dhruviii <93305785+602dhruviii@users.noreply.github.com>
2023-12-18 23:36:41 +0530

    express code updated
    express code updated

---
+++ Divyanshu_2111981072/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname)));
+
+// Set up a route to serve your HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
