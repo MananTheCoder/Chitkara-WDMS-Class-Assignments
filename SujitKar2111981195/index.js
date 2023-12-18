5f8ad2e02d54c735cf90cce5bdf0aad62a16d4c8
Sujit Kar <106083603+SujitKar1195@users.noreply.github.com>
2023-12-18 15:39:19 +0530

    Create index.js

---
+++ SujitKar2111981195/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen("3000", () => {
+  console.log("listening on port: 3000");
+});
