4ca24df29c0b1866dfa672e4c752b8e01edd1223
Ishani Thakur <ishani1251.be21@chitkarauniversity.edu.in>
2023-12-19 15:10:58 +0530

    some more

--- api/server.js
+++ api/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
+// Serve static files from the 'client' folder
+const clientPath = path.resolve(__dirname, '..', 'client');
+app.use(express.static(clientPath));
+
+// Handle requests for the root URL ('/')
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+  const indexPath = path.resolve(clientPath, 'index.html');
+  res.sendFile(indexPath);
 });
 
 app.listen(PORT, () => {
